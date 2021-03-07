4e0aa81b0c09cfbe871b9dd1dbacaffd59ce9818
willy <02320013apis@gmail.com>
2021-03-07 20:40:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+LeetCode
+========
+
+### LeetCode Algorithm
+
+| # | Title | My solution | Language | Difficulty | Runtime | Memory |
+|---| ----- | -------- | ---------- | ---------- | ------- | ------ |
+|771|[Jewels and Stones](https://leetcode.com/problems/jewels-and-stones/)|[Code]()|Java|Easy|72.94%|54.93%|
---
+++ solutions/771. Jewels and Stones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            String cur_stone = String.valueOf(stones.charAt(i));
+            if (jewels.contains(cur_stone)) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+}
