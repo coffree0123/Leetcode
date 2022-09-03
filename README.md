216711152fe2b4a22c278649607d4975168328d4
coffree <02320013apis@gmail.com>
2022-09-03 19:39:32 +0800

    test update

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ LeetCode
 
 | # | Title | My solution | Language | Difficulty |
 |---| ----- | -------- | ---------- | ---------- |
+|153|[Find Minimum in Rotated Sorted Array](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array)|[code](https://github.com/coffree0123/Leetcode/blob/master/solutions/153_Medium_Find%20Minimum%20in%20Rotated%20Sorted%20Array.cpp)|C++|Medium|
---
+++ solutions/153_Medium_Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        if (nums[0] < nums.back())
+            return nums[0];
+
+        int left = 0, right = nums.size()-1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            // Detect mid is on left side or right side
+            if (nums[mid] >= nums[0]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return nums[left];
+    }
+};
